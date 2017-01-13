b29b8538d94c078c20aac03662da8e25c39d2c23
Patrik Beyls <beylsp@yahoo>
2017-01-13 18:11:26 +0100

    Import mockredis for test configurations only

--- wonk/db.py
+++ wonk/db.py
@@ -1,15 +1,14 @@
 from flask_redis import FlaskRedis
-from mockredis import MockRedis
-
-
-class MockRedisWrapper(MockRedis):
-    @classmethod
-    def from_url(cls, *args, **kwargs):
-        return cls()
 
 
 def create_db(app):
     if app.testing:
+        from mockredis import MockRedis
+
+        class MockRedisWrapper(MockRedis):
+            @classmethod
+            def from_url(cls, *args, **kwargs):
+                return cls()
         db = FlaskRedis.from_custom_provider(MockRedisWrapper)
     else:
         db = FlaskRedis()
